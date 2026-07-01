e27a802f3117efb190e32711a8857022aca161de
karthikeya <karthikeya777666@gmail.com>
2026-07-01 19:07:21 -0400

    added so that the drawing is is optimal for the neural network

--- src/gui.rs
+++ src/gui.rs
@@ -40,19 +40,110 @@ impl eframe::App for DigitRecognizerApp {
                 }
 
                 if ui.button("Predict Digit").clicked() {
+                    let mut min_row = 28;
+                    let mut max_row = 0;
+                    let mut min_col = 28;
+                    let mut max_col = 0;
+                    let mut has_pixels = false;
+
+                    for row in 0..28 {
+                        for col in 0..28 {
+                            if self.grid[row][col] > 0.0 {
+                                if row < min_row { min_row = row; }
+                                if row > max_row { max_row = row; }
+                                if col < min_col { min_col = col; }
+                                if col > max_col { max_col = col; }
+                                has_pixels = true;
+                            }
+                        }
+                    }
+
+                    let mut final_grid = [[0.0; 28]; 28];
+
+                    if has_pixels {
+                        let box_height = (max_row - min_row + 1) as f32;
+                        let box_width = (max_col - min_col + 1) as f32;
+
+                        let target_dim = 20.0;
+                        let scale = (target_dim / box_height.max(box_width)).min(1.0);
+
+                        let mut scaled_grid = [[0.0; 28]; 28];
+
+                        // scale down the drwing
+                        for row in min_row..=max_row {
+                            for col in min_col..=max_col {
+                                let val = self.grid[row][col];
+                                if val > 0.0 {
+                                    // Map old coordinates relative to bounding box center, then scale
+                                    let rel_row = (row as f32 - min_row as f32) - (box_height / 2.0);
+                                    let rel_col = (col as f32 - min_col as f32) - (box_width / 2.0);
+
+                                    // Project onto the temporary grid centered at index 14
+                                    let target_row = (14.0 + rel_row * scale).round() as i32;
+                                    let target_col = (14.0 + rel_col * scale).round() as i32;
+
+                                    if target_row >= 0 && target_row < 28 && target_col >= 0 && target_col < 28 {
+                                        scaled_grid[target_row as usize][target_col as usize] = val;
+                                    }
+                                }
+                            }
+                        }
+
+                        // recenter
+                        let mut total_mass = 0.0;
+                        let mut sum_row = 0.0;
+                        let mut sum_col = 0.0;
+
+                        for row in 0..28 {
+                            for col in 0..28 {
+                                let val = scaled_grid[row][col];
+                                if val > 0.0 {
+                                    total_mass += val;
+                                    sum_row += row as f32 * val;
+                                    sum_col += col as f32 * val;
+                                }
+                            }
+                        }
+
+                        if total_mass > 0.0 {
+                            let center_row = sum_row / total_mass;
+                            let center_col = sum_col / total_mass;
+
+                            let shift_row = (13.5 - center_row).round() as i32;
+                            let shift_col = (13.5 - center_col).round() as i32;
+
+                            for row in 0..28 {
+                                for col in 0..28 {
+                                    let val = scaled_grid[row][col];
+                                    if val > 0.0 {
+                                        let t_row = row as i32 + shift_row;
+                                        let t_col = col as i32 + shift_col;
+
+                                        if t_row >= 0 && t_row < 28 && t_col >= 0 && t_col < 28 {
+                                            final_grid[t_row as usize][t_col as usize] = val;
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        final_grid = self.grid;
+                    }
+
+                    // flaten
                     let mut flattened = Vec::with_capacity(784);
                     for row in 0..28 {
                         for col in 0..28 {
-                            flattened.push(self.grid[row][col]);
+                            flattened.push(final_grid[row][col]);
                         }
                     }
 
                     let input_matrix = Array2::from_shape_vec((1, 784), flattened).unwrap();
 
                     let hidden1 = (input_matrix.dot(&self.layer1.weights) + &self.layer1.bias)
-                        .mapv(|x| if x > 0.0 { x } else { 0.0 });
+                        .mapv(|x| if x > 0.0 { x } else { x * 0.01 });
                     let hidden2 = (hidden1.dot(&self.layer2.weights) + &self.layer2.bias)
-                        .mapv(|x| if x > 0.0 { x } else { 0.0 });
+                        .mapv(|x| if x > 0.0 { x } else { x * 0.01 });
                     let output = hidden2.dot(&self.layer3.weights) + &self.layer3.bias;
 
                     let mut max_val = f32::MIN;
@@ -100,7 +191,6 @@ impl eframe::App for DigitRecognizerApp {
                 }
             }
 
-            // Draw grid cells onto the frame
             for row in 0..28 {
                 for col in 0..28 {
                     let val = self.grid[row][col];
